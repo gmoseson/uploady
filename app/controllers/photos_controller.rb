9ef312f6773d9b5d483b1043b216cd974a337ec7
Greta Moseson <gmoseson@gmail.com>
2015-03-30 20:25:50 -0400

    Add ability to upload and edit photos

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -17,6 +17,19 @@ class PhotosController < ApplicationController
   	end
   end
 
+  def edit
+    @photo = Photo.find(params[:id])
+  end
+
+  def update
+    @photo = Photo.find(params[:id])
+    if @photo.update_atributes(photo_params)
+      redirect_to photos_path
+    else 
+      render :edit
+    end
+  end
+
   private 
 	
   def photo_params
