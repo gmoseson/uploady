03349094d6f42d8992291c16ed5b4e1d9a3c6626
Greta Moseson <gmoseson@gmail.com>
2015-04-01 20:42:45 -0400

    Add commenting to photos.

--- app/tables/photos_table.rb
+++ app/tables/photos_table.rb
@@ -4,11 +4,21 @@ class PhotosTable < TableCloth::Base
   end
 
   column :caption
-
-  actions do
+  actions separator:  ' ' "|" " " do
     action do |photo|
       link_to 'Edit Photo', edit_photo_path(photo)
   end
+
+   action do |photo|
+      link_to 'Add Comment', new_photo_comment_path(photo)
+  end
+
+  action do |photo|
+     if photo.comments.any?
+      link_to 'View all comments', photo_comments_path(photo)
+  end
+end
+
 end
 
 config.table.class = "table table-bordered"
